30fc6b29476e7edfa0983457b4527e866179fcbc
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-16 16:38:48 +0000

    New background and LOGO added. Jin

--- trunk/footer.php
+++ trunk/footer.php
@@ -1,6 +1,7 @@
 ﻿</td>
 <tr>
-<td align="center" class="main_container">
+<td colspan="2" align="center">
+<hr  width="100%"/>
 <h3>This is footer part</h3>
 Copyright Reserved, Jin Wang and Jason Campbell, 2011
 </td>
