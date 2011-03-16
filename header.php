d6f0df93ba54de392caf991b1055fed77c1f1fcd
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-16 16:38:48 +0000

    New background and LOGO added. Jin

--- header.php
+++ header.php
@@ -3,21 +3,22 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
-<title>Forum</title>
+<title>J2 Forum</title>
 </head>
 
 <body>
 <table width="1000" align="center" class="main_container">
 <tr>
-<td align="center" class="main_container">
-<H1>Welcome to our forum!</H1>
+<td width="234"><a href="/"><img src="images/logo.png" width="234" height="120" /></a></td>
+<td width="766" align="center">
+<H1>Welcome to J2 Forum!</H1>
 <H2>This is header part</H2>
 </td>
 <tr>
 <tr>
-<td align="center" class="main_container">
-<H3>Navigation Bar</H3>
-<H4>Home | Forum | UCP</H4>
+<td colspan="2" align="center" class="main_container">
+<H3>Welcome back, ***!</H3>
+<H4><a href="/">Home</a> | <a href="/">Forum</a> | <a href="/">User Control Panel</a> | <a href="/help.php">Help</a> | <a href="/about.php">About Us</a></H4>
 </td>
 <tr>
-<td height="300" align="center" class="main_container">
+<td colspan="2" height="300" align="center">
