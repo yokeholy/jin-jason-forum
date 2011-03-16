2c1b7eb5d28a95ee4f59d77f5dc070cb71b5acd8
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-16 04:15:44 +0000

    Basic structure sketched. It now has a 4-row frame with header/navigation/main content/footer structure. And that's it for the first project day.

--- header.php
+++ header.php
@@ -2,8 +2,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
 <title>Forum</title>
 </head>
 
 <body>
+<table width="1000" align="center" class="main_container">
+<tr>
+<td align="center" class="main_container">
 <H1>Welcome to our forum!</H1>
+<H2>This is header part</H2>
+</td>
+<tr>
+<tr>
+<td align="center" class="main_container">
+<H3>Navigation Bar</H3>
+<H4>Home | Forum | UCP</H4>
+</td>
+<tr>
+<td height="300" align="center" class="main_container">
