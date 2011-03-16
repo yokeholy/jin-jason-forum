27331c54fa8422ddf121984f5d532fcac71c6f8a
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-16 04:15:44 +0000

    Basic structure sketched. It now has a 4-row frame with header/navigation/main content/footer structure. And that's it for the first project day.

--- trunk/footer.php
+++ trunk/footer.php
@@ -1,6 +1,9 @@
-﻿<table>
+﻿</td>
 <tr>
-<td>Copyright Reserved, 2011</td>
+<td align="center" class="main_container">
+<h3>This is footer part</h3>
+Copyright Reserved, Jin Wang and Jason Campbell, 2011
+</td>
 </tr>
 </table>
 
--- trunk/index.php
+++ trunk/index.php
@@ -2,13 +2,9 @@
 include("header.php");
 ?>
 
+<p>This is the content part.</p>
+<p>THIS WEBSITE IS ONLY A SKETCH NOW. I'M TOO SHY TO DISPLAY TOO MUCH INFORMATION AT THIS TIME. PLEASE COME BACK AND CHECK LATER.</p>
 
-<p>slfjsljdfldj
-	
-	
-	slkdfjskjdf </p>
-	
-slkfjslfjkd
 <?php
 include("footer.php");
 ?>
