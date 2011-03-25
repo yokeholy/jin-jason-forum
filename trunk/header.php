c3a77da446ac8b32b189a366b6514882bfbc1f8a
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 01:17:52 +0000

    Added user registration link to nav bar

--- trunk/header.php
+++ trunk/header.php
@@ -1,30 +1,35 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="css/styles.css" rel="stylesheet" type="text/css" />
-<title>J2 Forum</title>
-</head>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+		<title>J2 Forum</title>
+	</head>
 
-<body>
-<table width="1000" align="center" class="main_container">
-<tr>
-<td width="234"><a href="/"><img src="images/logo.png" width="234" height="120" /></a></td>
-<td width="766" align="center">
-<H1>Welcome to J2 Forum!</H1>
-<H2>This is header part</H2>
-</td>
-<tr>
-<tr>
-<td colspan="2" align="center" class="navigation_bar">
-<H3 class="navigation_bar">Welcome back, ***!</H3>
-<h4 class="navigation_bar"><a href="/" class="navigation_bar">Home</a> | <a href="/forum.php" class="navigation_bar">Forum</a> | <a href="/" class="navigation_bar">User Control Panel</a> | <a href="/help.php" class="navigation_bar">Help</a> | <a href="/about.php" class="navigation_bar">About Us</a></h4>
-<form name="Login" action="functions/login_action.php" method="post">
-Username:<input type="text" size="10" name="Username" />
-Password:<input type="password" size="10" name="Password" />
-<input type="submit" value="Login" />
-<input type="reset" value="Reset" />
-</form>
-</td>
-<tr>
-<td colspan="2" height="300" align="center">
+	<body>
+		<table width="1000" align="center" class="main_container">
+			<tr>
+				<td width="234"><a href="/"><img src="images/logo.png" width="234" height="120" /></a></td>
+				<td width="766" align="center">
+					<h1>Welcome to J2 Forum!</H1>
+					<h2>This is header part</H2>
+				</td>
+			<tr>
+
+			<tr>
+				<td colspan="2" align="center" class="navigation_bar">
+					<h3 class="navigation_bar">Welcome back, ***!</H3>
+					<h4 class="navigation_bar"><a href="/" class="navigation_bar">Home</a> | <a href="/forum.php" class="navigation_bar">Forum</a> | 
+						<a href="/" class="navigation_bar">User Control Panel</a> | <a href="/help.php" class="navigation_bar">Help</a> | 
+						<a href="/about.php" class="navigation_bar">About Us</a></h4>
+					<form name="Login" action="functions/login_action.php" method="post">
+						Username:<input type="text" size="10" name="Username" />
+						Password:<input type="password" size="10" name="Password" />
+						<input type="submit" value="Login" />
+						<input type="reset" value="Reset" />
+						<a href="/registration.php" class="navigation_bar">First Time User?</a>
+					</form>
+					
+				</td>
+			<tr>
+		<td colspan="2" height="300" align="center">
