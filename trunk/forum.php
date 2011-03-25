158667375a2e285915d4604e99bbcba54afec820
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 01:13:35 +0000

    Forum basic structure.

--- trunk/forum.php
+++ trunk/forum.php
@@ -1,6 +1,25 @@
 ﻿<?php 
 include("header.php");
+
+$Post['Subject'] = "Hello!";
+$Post['LastUser'] = "Jin";
+$Post['LastTime'] = date('d M Y H:i:s');
 ?>
+
+
+
+<table class="MainForum" width="90%" cellspacing="0" cellpadding="0">
+	<tr>
+		<td class="MainForum">Subject</td>
+		<td class="MainForum">Last Updated By</td>
+		<td class="MainForum">Last Updated Time</td>
+	</tr>
+	<tr>
+		<td class="MainForum"><?php echo $Post['Subject'];?></td>
+		<td class="MainForum"><?php echo $Post['LastUser'];?></td>
+		<td class="MainForum"><?php echo $Post['LastTime'];?></td>
+	</tr>
+</table>
 
 <?php
 include("footer.php");
