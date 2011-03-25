5839ce2d6176f1c6b488eb067560df70c9bcd40c
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 02:38:29 +0000

    Additional user registration updates and added instructions class to css file

--- registration.php
+++ registration.php
@@ -5,36 +5,33 @@ include("header.php");
 	<h1 style="text-align:left; margin-top:10px"><strong>First Time User Registration</strong></h1>
 	
 	<form name="Register" action="functions/registration_action.php" method="post" style="text-align:left">
-		<table width="500" align="left" class="registration">
+		<table width="100%" align="left" class="registration">
 			<tr>
 				<td width="100">Username: &nbsp;</td>
-				<td><input type="text" size="20" name="UserName" /> </td>
+				<td><input type="text" size="20" name="UserName" /> <span class="instructions"> (Maximum 20 characters) </span> </td>
 			</tr>
 			<tr>
 				<td>Password: &nbsp;</td>
-				<td><input type="password" size="20" name="Password" /> </td>
+				<td><input type="password" size="20" name="Password" /> <span class="instructions"> (Maximum 20 characters) </span>  </td>
 			</tr>
 			<tr>
 				<td>First Name: &nbsp;</td>
-				<td><input type="text" size="20" name="FirstName" /> </td>
+				<td><input type="text" size="20" name="FirstName" /> <span class="instructions"> (Maximum 20 characters) </span>  </td>
 			</tr>
 			<tr>
 				<td>Last Name: &nbsp;</td>
-				<td><input type="text" size="20" name="LastName" /> </td>
+				<td><input type="text" size="20" name="LastName" /> <span class="instructions"> (Maximum 20 characters) </span>  </td>
 			</tr>
 			<tr>
 				<td>Email Address: &nbsp;</td>
-				<td><input type="text" size="40" name="EmailAddr" /> </td>
+				<td><input type="text" size="40" name="EmailAddr" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters) </span>  </td>
 			</tr>
 			<tr>
 				<td colspan="2">
 					<input type="submit" value="Submit" />
 					<input type="reset" value="Reset" />
 				</td>
-			</tr>
-			
-			
-			
+			</tr>			
 		</table>
 	</form>
 
