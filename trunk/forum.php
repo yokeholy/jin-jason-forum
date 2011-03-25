0de6e881b2f6e4f3c6b521bca41dc0924b15dbbd
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 02:17:37 +0000

    Revised the Forum structure AGAIN.

--- trunk/forum.php
+++ trunk/forum.php
@@ -1,29 +1,29 @@
 ﻿<?php 
 include("header.php");
 
-$Post['Subject'] = "Hello!";
 $Post['LastUser'] = "Jin";
-$Post['LastTime'] = date('d M Y H:i:s');
 ?>
 
 
 
 <table class="MainForum" width="90%" cellspacing="0" cellpadding="0">
 	<tr valign="middle" onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
-		<td width="50%" valign="middle" class="MainForum"><p class="TableTitle">Subject</p></td>
-		<td width="35%" valign="middle" class="MainForum"><p class="TableTitle">Last Updated By</p></td>
-		<td width="15%" valign="middle" class="MainForum"><p class="TableTitle">Last Updated Time</p></td>
+		<td width="60%" valign="middle" class="MainForum"><p class="TableTitle">Subject</p></td>
+		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Last Updated</p></td>
+		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Original Posted</p></td>
 	</tr>
 
 <?php
 for($i = 1; $i <= 50; $i ++)
 {
+	$Post['Subject'] = "Hello! Here is to generate some random number: " . rand(10000000,0) . " - " . rand(10000000,0) . " - " . rand(10000000,0) . " - " . rand(10000000,0);
+	$Post['LastTime'] = date('d M Y H:i:s');
 ?>
 
 	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td class="MainForum"><p><strong><?php echo $Post['Subject'];?></strong></p></td>
-		<td class="MainForum"><p><?php echo $Post['LastUser'];?></p></td>
-		<td class="MainForum"><p><?php echo $Post['LastTime'];?></p></td>
+		<td class="MainForum" align="center"><p><?php echo $Post['LastUser'];?> <br />at <?php echo $Post['LastTime'];?></p></td>
+		<td class="MainForum" align="center"><p><?php echo $Post['LastUser'];?> <br />at <?php echo $Post['LastTime'];?></p></td>
 	</tr>
 	
 <?php
