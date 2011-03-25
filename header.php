9076be03ef3e2514291610db01bfe505b588ed0b
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 02:08:04 +0000

    initial first time user registration created

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php print('<?xml version = "1.0" encoding = "utf-8"?>') ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -32,4 +33,4 @@
 					
 				</td>
 			<tr>
-		<td colspan="2" height="300" align="center">
+			<td colspan="2"  align="center">
--- registration.php
+++ registration.php
@@ -2,6 +2,43 @@
 include("header.php");
 ?>
 
+	<h1 style="text-align:left; margin-top:10px"><strong>First Time User Registration</strong></h1>
+	
+	<form name="Register" action="functions/registration_action.php" method="post" style="text-align:left">
+		<table width="500" align="left" class="registration">
+			<tr>
+				<td width="100">Username: &nbsp;</td>
+				<td><input type="text" size="20" name="UserName" /> </td>
+			</tr>
+			<tr>
+				<td>Password: &nbsp;</td>
+				<td><input type="password" size="20" name="Password" /> </td>
+			</tr>
+			<tr>
+				<td>First Name: &nbsp;</td>
+				<td><input type="text" size="20" name="FirstName" /> </td>
+			</tr>
+			<tr>
+				<td>Last Name: &nbsp;</td>
+				<td><input type="text" size="20" name="LastName" /> </td>
+			</tr>
+			<tr>
+				<td>Email Address: &nbsp;</td>
+				<td><input type="text" size="40" name="EmailAddr" /> </td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="Submit" />
+					<input type="reset" value="Reset" />
+				</td>
+			</tr>
+			
+			
+			
+		</table>
+	</form>
+
+
 <?php
 include("footer.php");
 ?>
