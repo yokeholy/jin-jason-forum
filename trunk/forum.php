0729ce7dca2222518a13e45e9d99aa55f425713a
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-25 01:54:48 +0000

    style revised for Forum structure

--- trunk/forum.php
+++ trunk/forum.php
@@ -9,16 +9,26 @@ $Post['LastTime'] = date('d M Y H:i:s');
 
 
 <table class="MainForum" width="90%" cellspacing="0" cellpadding="0">
-	<tr>
-		<td class="MainForum">Subject</td>
-		<td class="MainForum">Last Updated By</td>
-		<td class="MainForum">Last Updated Time</td>
+	<tr valign="middle" onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
+		<td width="50%" valign="middle" class="MainForum"><p class="TableTitle">Subject</p></td>
+		<td width="35%" valign="middle" class="MainForum"><p class="TableTitle">Last Updated By</p></td>
+		<td width="15%" valign="middle" class="MainForum"><p class="TableTitle">Last Updated Time</p></td>
 	</tr>
-	<tr>
-		<td class="MainForum"><?php echo $Post['Subject'];?></td>
-		<td class="MainForum"><?php echo $Post['LastUser'];?></td>
-		<td class="MainForum"><?php echo $Post['LastTime'];?></td>
+
+<?php
+for($i = 1; $i <= 50; $i ++)
+{
+?>
+
+	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
+		<td class="MainForum"><p><strong><?php echo $Post['Subject'];?></strong></p></td>
+		<td class="MainForum"><p><?php echo $Post['LastUser'];?></p></td>
+		<td class="MainForum"><p><?php echo $Post['LastTime'];?></p></td>
 	</tr>
+	
+<?php
+}
+?>
 </table>
 
 <?php
