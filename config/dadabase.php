04af2e3ff641388f08a0d3dd4416fe338e9a648c
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-03-30 01:38:18 +0000

    Database connection created.

---
+++ config/dadabase.php
@@ -0,0 +1,4 @@
+<?php
+	mysql_connect("localhost","root") or die('Cannot connect to database');
+	mysql_select_db("forum") or die('Cannot select database');
+?>
--- header.php
+++ header.php
@@ -1,4 +1,7 @@
-﻿<?php print('<?xml version = "1.0" encoding = "utf-8"?>') ?>
+﻿<?php
+include ('config/dadabase.php');
+?>
+<?xml version = "1.0" encoding = "utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
