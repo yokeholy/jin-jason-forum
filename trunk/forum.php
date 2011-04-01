ff64dff6bec4a404826d76c6e0fbd2a97c320f9c
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-01 00:46:14 +0000

    Now can correctly list the threads in forum.php

--- trunk/forum.php
+++ trunk/forum.php
@@ -9,33 +9,43 @@ $query = mysql_query("SELECT * FROM posts WHERE PostType = 0 LIMIT 0, 25");
 <table class="MainForum" width="90%" cellspacing="0" cellpadding="0">
 	<tr valign="middle" onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td width="60%" valign="middle" class="MainForum"><p class="TableTitle">Subject</p></td>
-		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Last Updated</p></td>
 		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Original Posted</p></td>
+		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Last Updated</p></td>
 	</tr>
 
 <?php
 while($result = mysql_fetch_array($query))
 {
-	$queryUser = mysql_query("SELECT * FROM users WHERE UserID = ".$result['UserID']);
+	$queryUser = mysql_query("SELECT * FROM users WHERE UserID = '".$result['UserID']."'");
 	$resultUser = mysql_fetch_array($queryUser);
 	$PostID = $result['PostID'];
-	$queryReply = mysql_query("SELECT * FROM posts WHERE ThreadID = '".$PostID."' ORDER BY 'PostID' DESC LIMIT 0, 1");
-	if($queryReply)
-	{
+	$queryReply = mysql_query("SELECT * FROM posts WHERE ThreadID = '$PostID' ORDER BY 'PostID' DESC LIMIT 0, 1");
 	$resultReply = mysql_fetch_array($queryReply);
-	$queryReplyUser = mysql_query("SELECT * FROM users WHERE UserID = ".$resultReply['UserID']);
+	if($resultReply)
+	{
+	$queryReplyUser = mysql_query("SELECT * FROM users WHERE UserId = '".$resultReply['UserID']."'");
 	$resultReplyUser = mysql_fetch_array($queryReplyUser);
-	}
-
 ?>
 
 	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td class="MainForum"><p><strong><?php echo $result['Subject'];?></strong></p></td>
 		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
-		<td class="MainForum" align="center"><p>by <strong><?php echo $resultReplyUser['UserName'];?></strong> <br />at <?php echo $resultReply['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultReplyUser['UserName'];?></strong> <br />at <?php echo $resultReply['PostDate']." ".$resultReply['PostTime'];?></p></td>
 	</tr>
 	
 <?php
+	}
+	else
+	{
+?>
+	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
+		<td class="MainForum"><p><strong><?php echo $result['Subject'];?></strong></p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+	</tr>
+
+<?php
+	}
 }
 ?>
 </table>
