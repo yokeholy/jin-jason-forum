8112c01b3f03ce0254a0c006ba58abd080cb8bd3
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-01 01:53:00 +0000

    this is the correct version of MySQL

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include ('config/database.php');
+include ('/config/database.php');
 ?>
 <?xml version = "1.0" encoding = "utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 include("header.php");
 ?>
 
-<p>This is the content part.</p>
-<p>THIS WEBSITE IS ONLY A SKETCH NOW. I'M TOO SHY TO DISPLAY TOO MUCH INFORMATION AT THIS TIME. PLEASE COME BACK AND CHECK LATER.</p>
+<h1>Welcome to J2 Forum.</h1>
+<h2>created by Jin Wang and Jason Campbell</h2>
+<p>Functions that are working now:<br />
+* User Registration *<br />
+* Forum threads list
+*</p>
+
 
 <?php
 include("footer.php");
