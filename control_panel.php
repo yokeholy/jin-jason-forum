8d5ac4db4f68c865ce9bf9c3bf9ece0c501ea7e1
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-04 00:11:20 +0000

    updated registration_login.php to have Confirm Password field and functionality

--- control_panel.php
+++ control_panel.php
@@ -1,9 +1,14 @@
-<?php 
+<?php
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 ?>
 
-	<h1 style="text-align:left; margin-top:10px"><strong>First Time User Registration</strong></h1>
-	<div class="instructions2" style="text-align:left; margin-top:10px"/>Please fill in all fields below and click Submit.
+<?php
+	extract($_POST);
+?>
+
+	<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
+	<h2 style="text-align:left; margin-top:10px">Welcome, FirstName!  You can change your preferences here.</h2>
+	<div class="instructions2" style="text-align:left; margin-top:10px"/>Change preferences here.
 		&nbsp;Click Reset to clear all fields.</div><br />
 	<form name="Register" action="registration_action.php" method="post" style="text-align:left">
 		<table width="100%" align="left" class="registration">
