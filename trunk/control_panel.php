aab18bf118e7b09ec42ed8f261a63d93780df9f6
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-06 05:00:47 +0000

    updated control_panel.php to include some options but more work is needed

--- trunk/control_panel.php
+++ trunk/control_panel.php
@@ -6,32 +6,68 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 	extract($_POST);
 ?>
 
+
 	<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
-	<h2 style="text-align:left; margin-top:10px">Welcome, FirstName!  You can change your preferences here.</h2>
-	<div class="instructions2" style="text-align:left; margin-top:10px"/>Change preferences here.
-		&nbsp;Click Reset to clear all fields.</div><br />
-	<form name="Register" action="registration_action.php" method="post" style="text-align:left">
-		<table width="100%" align="left" class="registration">
+	<h3 style="text-align:left; margin-top:10px">Welcome, FirstName!  You can change your preferences here.</h3>
+	
+	<div class="control_panel" style="text-align:left; margin-top:10px"/>Current User Info:</div>
+	<table width="50%" align="left" class="control_panel">
+		<tr>
+			<td>Username: &nbsp;</td>
+			<td>How do I put $UserName here?</td>
+		</tr>
+		<tr>
+			<td>Email Address: &nbsp;</td>
+			<td>How do I put $EmailAddr here?</td>
+		</tr>
+		<tr>
+			<td>Registration Date: &nbsp;</td>
+			<td>How do I put $RegDate here?</td>
+		</tr>
+		<tr>
+			<td>Total Posts: &nbsp;</td>
+			<td>How do I put $TotalPosts here?</td>
+		</tr>		
+		
+	
+	<form name="ChangeOptions" action="ControlPanel_action.php" method="post" style="text-align:left">
+		<table width="100%" align="left" class="UserOptions">
 			<tr>
-				<td width="100">Username: &nbsp;</td>
-				<td><input name="UserName" type="text" size="20" maxlength="20" /> <span class="instructions"> (3 to 20 characters) </span> </td>
+				<td width="200">Threads per page: &nbsp;</td>
+				<td><select name="ThreadsPerPage" 
+						<option selected = "selected">10</option>
+						<option>25</option>
+						<option>50</option>
+						<option>100</option>
+					</select>
+				</td>
 			</tr>
 			<tr>
-				<td>Password: &nbsp;</td>
-				<td><input name="Password" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters) </span>  </td>
+				<td>Thread Order: &nbsp;</td>
+				<td><label>Newest First <input name="ThreadOrder" type="checkbox" value="NewestFirst"/> </label>
+					<label>Oldest First <input name="ThreadOrder" type="checkbox" value="OldestFirst"/> </label></td>
 			</tr>
 			<tr>
-				<td>First Name: &nbsp;</td>
-				<td><input name="FirstName" type="text" size="20" maxlength="20" /> <span class="instructions"> (2 to 20 characters) </span>  </td>
+				<td>Change Password: &nbsp;</td>
+				<td><input name="ChangePassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
 			</tr>
 			<tr>
-				<td>Last Name: &nbsp;</td>
-				<td><input name="LastName" type="text" size="20" maxlength="20" /> <span class="instructions"> (2 to 20 characters) </span>  </td>
+				<td>Confirm Password: &nbsp;</td>
+				<td><input name="ConfirmPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
 			</tr>
 			<tr>
-				<td>Email Address: &nbsp;</td>
-				<td><input name="EmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters) </span>  </td>
+				<td>Change Email Address? &nbsp;</td>
+				<td><input name="ChangeEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
 			</tr>
+			<tr>
+				<td>Confirm Email Address: &nbsp;</td>
+				<td><input name="ConfirmEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
+			</tr>
+			<tr>
+				<td>Want to receive email updates? &nbsp;</td>
+				<td><label>Check here <input name="EmailUpdates" type="checkbox" value="Yes" </label></td>
+			</tr>
+
 			<tr>
 				<td colspan="2">
 					<input type="submit" value="Submit" />
