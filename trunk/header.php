bacd75475fbd97a78c616d3055cc5b3f57696df1
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-13 03:18:46 +0000

    Header now can determine whether user is logged in. And displays differently for navigation bar.

--- trunk/header.php
+++ trunk/header.php
@@ -24,10 +24,18 @@ session_start();
 
 			<tr>
 				<td colspan="2" align="center" class="navigation_bar">
-					<h3 class="navigation_bar">Welcome back, ***!</H3>
-					<h4 class="navigation_bar"><a href="/" class="navigation_bar">Home</a> | <a href="/forum.php" class="navigation_bar">Forum</a> | 
-						<a href="/control_panel.php" class="navigation_bar">User Control Panel</a> | <a href="/help.php" class="navigation_bar">Help</a> | 
-						<a href="/about.php" class="navigation_bar">About Us</a></h4>
+<?php
+if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
+{
+?>
+					<h3 class="navigation_bar">Welcome back, <?php echo $_SESSION['UserName'] ?>!</H3>
+					<h4 class="navigation_bar"><a href="/" class="navigation_bar">Home</a> | <a href="/forum.php" class="navigation_bar">Forum</a> | <a href="/control_panel.php" class="navigation_bar">User Control Panel</a> | <a href="/help.php" class="navigation_bar">Help</a> | <a href="/about.php" class="navigation_bar">About Us</a></h4>
+<?php 
+} else
+{
+?>
+					<h4 class="navigation_bar"><a href="/" class="navigation_bar">Home</a> | <a href="/forum.php" class="navigation_bar">Forum</a> | <a href="/help.php" class="navigation_bar">Help</a> | <a href="/about.php" class="navigation_bar">About Us</a></h4>
+
 					<form name="Login" action="/functions/login_action.php" method="post">
 						Username:<input name="UserName" type="text" size="10" maxlength="20" />
 						Password:<input name="Password" type="password" size="10" maxlength="20" />
@@ -35,6 +43,9 @@ session_start();
 						<input type="reset" value="Reset" />
 						<a href="/registration.php" class="navigation_bar">First Time User?</a>
 					</form>
+<?php
+}
+?>
 					
 				</td>
 			<tr>
