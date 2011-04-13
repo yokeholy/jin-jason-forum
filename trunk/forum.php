d448eab3ef5fdf375f141889d9b4769dcdb619f0
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-13 02:17:03 +0000

    Sticky posts are working!

--- trunk/forum.php
+++ trunk/forum.php
@@ -1,7 +1,5 @@
 ﻿<?php 
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-//query from database
-$query = mysql_query("SELECT * FROM posts WHERE PostType = 0 LIMIT 0, 25");
 ?>
 
 <p align="left"><a style="font-size:16px; margin:10px; height:20px;" href="newpost.php">Add a Thread</a></p>
@@ -9,17 +7,60 @@ $query = mysql_query("SELECT * FROM posts WHERE PostType = 0 LIMIT 0, 25");
 <table class="MainForum" width="90%" cellspacing="0" cellpadding="0">
 	<tr valign="middle" onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td width="60%" valign="middle" class="MainForum"><p class="TableTitle">Subject</p></td>
-		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Original Posted</p></td>
-		<td width="20%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Last Updated</p></td>
+		<td width="18%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Original Posted</p></td>
+		<td width="5%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Reply</p></td>
+		<td width="17%" valign="middle" class="MainForum" align="center"><p class="TableTitle">Last Updated</p></td>
 	</tr>
 
 <?php
+//query for sticky posts from database
+$query = mysql_query("SELECT * FROM posts WHERE PostType = 0 AND Sticky = 1");
 while($result = mysql_fetch_array($query))
 {
 	$queryUser = mysql_query("SELECT * FROM users WHERE UserID = '".$result['UserID']."'");
 	$resultUser = mysql_fetch_array($queryUser);
 	$PostID = $result['PostID'];
-	$queryReply = mysql_query("SELECT * FROM posts WHERE ThreadID = '$PostID' ORDER BY 'PostID' DESC LIMIT 0, 1");
+	$queryReply = mysql_query("SELECT * FROM posts WHERE ThreadID = '$PostID' AND PostID != '$PostID' ORDER BY 'PostID' ASC LIMIT 0, 1");
+	$resultReply = mysql_fetch_array($queryReply);
+	if($resultReply)
+	{
+	$queryReplyUser = mysql_query("SELECT * FROM users WHERE UserId = '".$resultReply['UserID']."'");
+	$resultReplyUser = mysql_fetch_array($queryReplyUser);
+?>
+
+	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
+		<td class="MainForum"><p><strong><a href="/viewthread.php?tid=<?php echo $result['PostID']?>"><span class="Sticky"><?php echo $result['Subject'];?></span></a></strong></p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>some</p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultReplyUser['UserName'];?></strong> <br />at <?php echo $resultReply['PostDate']." ".$resultReply['PostTime'];?></p></td>
+	</tr>
+	
+<?php
+	}
+	else
+	{
+?>
+	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
+		<td class="MainForum"><p><strong><a href="/viewthread.php?tid=<?php echo $result['PostID']?>"><span class="Sticky"><?php echo $result['Subject'];?></span></a></strong></p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>0</p></td>
+		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+	</tr>
+
+<?php
+	}
+}
+
+
+
+//query for regular posts from database
+$query = mysql_query("SELECT * FROM posts WHERE PostType = 0 AND Sticky = 0 LIMIT 0, 25");
+while($result = mysql_fetch_array($query))
+{
+	$queryUser = mysql_query("SELECT * FROM users WHERE UserID = '".$result['UserID']."'");
+	$resultUser = mysql_fetch_array($queryUser);
+	$PostID = $result['PostID'];
+	$queryReply = mysql_query("SELECT * FROM posts WHERE ThreadID = '$PostID' AND PostID != '$PostID' ORDER BY 'PostID' DESC LIMIT 0, 1");
 	$resultReply = mysql_fetch_array($queryReply);
 	if($resultReply)
 	{
@@ -30,6 +71,7 @@ while($result = mysql_fetch_array($query))
 	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td class="MainForum"><p><strong><a href="/viewthread.php?tid=<?php echo $result['PostID']?>"><?php echo $result['Subject'];?></a></strong></p></td>
 		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>some</p></td>
 		<td class="MainForum" align="center"><p>by <strong><?php echo $resultReplyUser['UserName'];?></strong> <br />at <?php echo $resultReply['PostDate']." ".$resultReply['PostTime'];?></p></td>
 	</tr>
 	
@@ -41,6 +83,7 @@ while($result = mysql_fetch_array($query))
 	<tr onmouseover="style.backgroundColor='#ddd'" onmouseout="style.backgroundColor='#fff'">
 		<td class="MainForum"><p><strong><a href="/viewthread.php?tid=<?php echo $result['PostID']?>"><?php echo $result['Subject'];?></a></strong></p></td>
 		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
+		<td class="MainForum" align="center"><p>0</p></td>
 		<td class="MainForum" align="center"><p>by <strong><?php echo $resultUser['UserName'];?></strong> <br />at <?php echo $result['PostDate']." ".$result['PostTime'];?></p></td>
 	</tr>
 
