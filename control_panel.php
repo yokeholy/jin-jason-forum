ac59414ad14823708664cae7c8d0a61358c97ab2
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-15 03:25:42 +0000

    change password funcion hopefully working in control_panel_action.php

--- control_panel.php
+++ control_panel.php
@@ -38,18 +38,24 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 		$dateAndTime = "<p>$year-$month-$day  $hours:$minutes:$seconds</p>";
 		print("Last login: " . $LastLogin);
 		
+		// get Total Posts to display in User Control Panel
 		$queryGetTotalPosts = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."'";
 		$result = mysql_query($queryGetTotalPosts);
 		$numRows = mysql_fetch_array($result);
 		$TotalPosts = $numRows[0];
-		
-		//print($numRows[0]  . "Rows\n");
+
+		// get Threads Started to display in User Control Panel
+		$queryGetThreadsStarted = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."' AND PostType = 0";
+		$result = mysql_query($queryGetThreadsStarted);
+		$numRows = mysql_fetch_array($result);
+		$ThreadsStarted = $numRows[0];
+
 
 ?>
 
 
 		<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
-		<h3 style="text-align:left; margin-top:10px">Welcome, <?php print($UserName);?>!  You can change your preferences here.</h3>
+		<h3 style="text-align:left; margin-top:10px">Welcome, <?php print($UserName);?>!  You can change view and change your info here.</h3>
 		
 		<div class="control_panel" style="text-align:left; margin-top:10px"/>Current User Info:</div>
 		<form name="ChangeOptions" action="control_panel_action.php" method="post" style="text-align:left">
@@ -74,51 +80,73 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 				<td>Total Posts: &nbsp;</td>
 				<td><?php print($TotalPosts);?></td>
 			</tr>		
-			
-		
-				<tr>
-					<td width="200">Threads per page: &nbsp;</td>
-					<td><select name="ThreadsPerPage" >
-							<option selected = "selected">10</option>
-							<option>25</option>
-							<option>50</option>
-							<option>100</option>
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<td>Thread Order: &nbsp;</td>
-					<td><label>Newest First <input name="ThreadOrder" type="checkbox" value="NewestFirst"/> </label>
-						<label>Oldest First <input name="ThreadOrder" type="checkbox" value="OldestFirst"/> </label></td>
-				</tr>
-				<tr>
-					<td>Change Password: &nbsp;</td>
-					<td><input name="ChangePassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
-				</tr>
-				<tr>
-					<td>Confirm Password: &nbsp;</td>
-					<td><input name="ConfirmPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
-				</tr>
-				<tr>
-					<td>Change Email Address? &nbsp;</td>
-					<td><input name="ChangeEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
-				</tr>
-				<tr>
-					<td>Confirm Email Address: &nbsp;</td>
-					<td><input name="ConfirmEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
-				</tr>
-				<tr>
-					<td>Want to receive email updates? &nbsp;</td>
-					<td><label>Check here <input name="EmailUpdates" type="checkbox" value="Yes"  /></label></td>
-				</tr>
+			<tr>
+				<td>Threads Started: &nbsp;</td>
+				<td><?php print($ThreadsStarted);?></td>
+			</tr>			
+			<tr>
+				<td width="200">Threads per page: &nbsp;</td>
+				<td><select name="ThreadsPerPage" >
+						<option selected = "selected">10</option>
+						<option>25</option>
+						<option>50</option>
+						<option>100</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Thread Order: &nbsp;</td>
+				<td><label>Newest First <input name="ThreadOrder" type="checkbox" value="NewestFirst"/> </label>
+					<label>Oldest First <input name="ThreadOrder" type="checkbox" value="OldestFirst"/> </label></td>
+			</tr>
+			<tr>
+				<td><br/></td>
+			</tr>
+			<tr>
+				<td><u>Change Password?</u></td>
+			</tr>
+			<tr>
+				<td>Old Password: &nbsp;</td>
+				<td><input name="OldPassword" type="password" size="20" maxlength="20" /> <span class="instructions"></span></td>
+			</tr>
+			<tr>
+				<td>Confirm Old Password: &nbsp;</td>
+				<td><input name="ConfirmOldPassword" type="password" size="20" maxlength="20" /> <span class="instructions"></span></td>
+			</tr>
+			<tr>
+				<td>New Password: &nbsp;</td>
+				<td><input name="NewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
+			</tr>
+			<tr>
+				<td>Confirm New Password: &nbsp;</td>
+				<td><input name="ConfirmNewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
+			</tr>
+			<tr>
+				<td><br/></td>
+			</tr>
+			<tr>
+				<td><u>Change Email Address?</u></td>
+			</tr>
+			<tr>
+				<td>New Email Address: &nbsp;</td>
+				<td><input name="NewEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
+			</tr>
+			<tr>
+				<td>Confirm New Email Address: &nbsp;</td>
+				<td><input name="ConfirmNewEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
+			</tr>
+			<tr>
+				<td>Want to receive email updates? &nbsp;</td>
+				<td><label>Check here <input name="EmailUpdates" type="checkbox" value="Yes"  /></label></td>
+			</tr>
 
-				<tr>
-					<td colspan="2">
-						<input type="submit" value="Submit" />
-						<input type="reset" value="Reset" />
-					</td>
-				</tr>			
-			</table>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="Submit" />
+					<input type="reset" value="Reset" />
+				</td>
+			</tr>			
+		</table>
 		</form>
 
 	
