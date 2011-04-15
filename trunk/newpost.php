cb56c54c85756fb40c023dd86dd4cfa4f1d61ba5
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-15 03:06:54 +0000

    ABLE TO ADD A THREAD OR REPLY NOW, fixed threads display error in forum.php

--- trunk/newpost.php
+++ trunk/newpost.php
@@ -1,22 +1,26 @@
 ﻿<?php 
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-if(isset($_GET['NewType']) && $_GET['NewType'] == 1) // If the new post is a reply to an existing thread. NewType 1 means that this post is a reply and will cause the page to show the New Reply interface. Otherwise, it's gonna show the New Thread interface.
+extract($_POST);
+if(isset($NewType) && $NewType == 1) // If the new post is a reply to an existing thread. NewType 1 means that this post is a reply and will cause the page to show the New Reply interface. Otherwise, it's gonna show the New Thread interface.
 {
 ?>
 
 <h1 align="left">New Reply</h1>
 <form action="/functions/addpost_action.php" method="post">
+<input name="NewType" type="hidden" value="1" />
+<input name="tid" type="hidden" value="<?php echo $tid ?>" />
+
 	<table cellpadding="0" cellspacing="0" class="AddPostTable">
 		<tr>
 			<td class="AddPost"><p><strong>Subject:</strong></p></td>
-			<td class="AddPost"><input name="Subject" size="75"></td>
+			<td class="AddPost"><input name="Subject" size="75" value="Re: <?php echo $subject ?>" /></td>
 		</tr>
 		<tr>
 			<td class="AddPost"><p><strong>Content:</strong></p></td>
 			<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
 		</tr>
 		<tr align="center">
-			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post!"></td>
+			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post the Reply!" /><input type="reset" value="No, clean them for me"></td>
 		</tr>
 	</table>
 </form>
@@ -27,6 +31,7 @@ else
 ?>
 <h1 align="left">New Thread</h1>
 <form action="/functions/addpost_action.php" method="post">
+<input name="NewType" type="hidden" value="0" />
 	<table cellpadding="0" cellspacing="0" class="AddPostTable">
 		<tr>
 			<td class="AddPost"><p><strong>Subject:</strong></p></td>
@@ -37,7 +42,7 @@ else
 			<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
 		</tr>
 		<tr align="center">
-			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post it RIGHT NOW!"><input type="reset" value="No, clean them for me"></td>
+			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post the New Thread!"><input type="reset" value="No, clean them for me"></td>
 		</tr>
 	</table>
 </form>
