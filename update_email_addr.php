b596f6a6ea0f45af4cd772950350322e55a29a19
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-17 23:04:08 +0000

    revamped change password and change email address.  Both seem to be working correctly now except for receiving email updates checkbox

--- update_email_addr.php
+++ update_email_addr.php
@@ -3,113 +3,14 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 //session_start();
 ?>
 
-<?php
-	
-	
-	if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
-	{
-				
-		$UserName = $_SESSION['UserName'];
-		$Password = $_SESSION['Password'];
-		$EmailAddr = $_SESSION['EmailAddr'];
-		$OrigSignup = $_SESSION['OrigSignup'];
+		<h1 style="text-align:left; margin-top:10px"><strong>Change Email Address</strong></h1>
+		<h3 style="text-align:left; margin-top:10px">You can update your preferred email address on file.</h3>
 		
-		$UserID = $_SESSION['UserID'];
-		$LastLogin = $_SESSION['LastLogin'];
-		//echo $UserName;
-		//echo $Password;
-		//echo $EmailAddr;
-		
-	//	$getEmailAddr = "SELECT EmailAddr FROM users WHERE UserName = '".$UserName."'";
-	//	$EmailAddr = mysql_fetch_row(mysql_query($getEmailAddr));
-
-	// get date and time:
-		$now = getdate();
-
-		$year = $now['year'];
-		$month = $now['mon'];
-		$day = $now['mday'];
-		$hours = $now['hours'];
-		$minutes = $now['minutes'];
-		$seconds = $now['seconds'];
-		
-		//print("<p>$year-$month-$day  $hours:$minutes:$seconds</p>");
-
-		$dateAndTime = "<p>$year-$month-$day  $hours:$minutes:$seconds</p>";
-		print("Last login: " . $LastLogin);
-		
-		// get Total Posts to display in User Control Panel
-		$queryGetTotalPosts = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."'";
-		$result = mysql_query($queryGetTotalPosts);
-		$numRows = mysql_fetch_array($result);
-		$TotalPosts = $numRows[0];
-
-		// get Threads Started to display in User Control Panel
-		$queryGetThreadsStarted = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."' AND PostType = 0";
-		$result = mysql_query($queryGetThreadsStarted);
-		$numRows = mysql_fetch_array($result);
-		$ThreadsStarted = $numRows[0];
-
-
-?>
-
-
-		<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
-		<h3 style="text-align:left; margin-top:10px">Welcome, <?php print($UserName);?>!  You can change view and change your info here.</h3>
-		
-		<div class="control_panel" style="text-align:left; margin-top:10px"/>Current User Info:</div>
-		<form name="ChangeOptions" action="control_panel_action.php" method="post" style="text-align:left">
+		<form name="ChangeEmailAddr" action="update_email_addr_action.php" method="post" style="text-align:left">
 		<table width="100%" align="left" class="control_panel">
 			<tr>
-				<td>Username: &nbsp;</td>
-				<td><?php print($UserName);?></td>
-			</tr>
-			<tr>
-				<td>Email Address: &nbsp;</td>
-				<td><?php print($EmailAddr);?></td>
-			</tr>
-			<tr>
-				<td>Registration Date/Time (GMT): &nbsp;</td>
-				<td><?php print($OrigSignup);?></td>
-			</tr>
-			<tr>
-				<td>Last Login (GMT): &nbsp;</td>
-				<td><?php print($LastLogin);?></td>
-			</tr>
-			<tr>
-				<td>Total Posts: &nbsp;</td>
-				<td><?php print($TotalPosts);?></td>
-			</tr>		
-			<tr>
-				<td>Threads Started: &nbsp;</td>
-				<td><?php print($ThreadsStarted);?></td>
-			</tr>			
-			<tr>
-				<td>Threads per page: &nbsp;</td>
-				<td><select name="ThreadsPerPage" >
-						<option selected = "selected">10</option>
-						<option>25</option>
-						<option>50</option>
-						<option>100</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>Thread Order: &nbsp;</td>
-				<td><label>Newest First <input name="ThreadOrder" type="checkbox" value="NewestFirst"/> </label>
-					<label>Oldest First <input name="ThreadOrder" type="checkbox" value="OldestFirst"/> </label></td>
-			</tr>
-			<tr>
-				<td><br/></td>
-			</tr>
-			<tr>
-				<td><a href="/update_password.php" class="change_password">Change Password? (click here)</a></td>
-			</tr>
-			<tr>
-				<td><br/></td>
-			</tr>
-			<tr>
-				<td width="220"><a href="/update_email_addr.php" class="update_email_addr">Update Email Address? (click here)</a></td>
+				<td>Confirm Old Password: &nbsp;</td>
+				<td><input name="ConfirmOldPassword" type="password" size="20" maxlength="20" /> <span class="instructions"></span></td>
 			</tr>
 			<tr>
 				<td>New Email Address: &nbsp;</td>
@@ -120,7 +21,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 				<td><input name="ConfirmNewEmailAddr" type="text" size="40" maxlength="40" /> <span class="instructions"> (Ex. yourname@domain.com) (Maximum 40 characters)</span></td>
 			</tr>
 			<tr>
-				<td>Want to receive email updates? &nbsp;</td>
+				<td width="200">Want to receive email updates? &nbsp;</td>
 				<td><label>Check here <input name="EmailUpdates" type="checkbox" value="Yes"  /></label></td>
 			</tr>
 
@@ -133,9 +34,6 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 		</table>
 		</form>
 
-	
-
 <?php
-} // end if (checking if LoggedIn is valid)
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
 ?>
