a7fd38f485a885f6a5b983a99af7227777bb7b7e
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-19 05:20:11 +0000

    updated about.php with some info.  Jin still needs to fill out his section

--- about.php
+++ about.php
@@ -3,7 +3,15 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 ?>
 
 <h1>About Us</h1>
-<p>Some descriptions of the project and ourselves.</p>
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Jin Wang</strong> is (Jin fill this in)</p><br/><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Jason Campbell</strong> is a student in Computer Science at Troy University.  
+	He has a degree in Computer Engineering from North Carolina State University and has worked in computer hardware design at IBM.  
+	Back in his home town of Troy, AL, he is seeking a second degree to follow a more software oriented career.
+	Jason enjoys spending time with his wife and daughter, video games, guitar, tennis, golf, and of course, computer programming!</p><br/><br/>
+	
+<p class = "about" style="text-align:left; margin-top:10px">Together, <strong>Jin and Jason</strong> decided to put their skills to the test and create an
+	open forum to discuss any subject material comprehensive of all human knowledge!</p>
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
