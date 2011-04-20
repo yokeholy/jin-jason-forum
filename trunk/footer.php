1b1287301512950542a1fe5eb61ef32631c3f731
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-20 03:39:25 +0000

    updated header and footer content

--- trunk/footer.php
+++ trunk/footer.php
@@ -2,9 +2,7 @@
 <tr>
 <td colspan="2" align="center">
 <hr  width="100%"/>
-<h3>This is footer part</h3>
-<p>Copyright Reserved, Jin Wang and Jason Campbell, 2011</p>
-</td>
+<p>Copyright Reserved, Jin Wang and Jason Campbell, 2011</p></td>
 </tr>
 </table>
 
