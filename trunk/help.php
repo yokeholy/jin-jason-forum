1c0c91d9217c95bde52ccca2a0d0126414192de0
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-20 02:25:04 +0000

    help page updated but can't figure out font issue

--- trunk/help.php
+++ trunk/help.php
@@ -3,7 +3,26 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 ?>
 
 <h1>Help</h1>
-<p>Currently not available.</p>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q & A:</strong></p><br/><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> Can I view forum posts without being a registered user?<br/>
+<strong>A:</strong> Of course!  Feel free to view and read as many posts as you would like.</p><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> Can I reply to a post without being a registered user?<br/>
+<strong>A:</strong> No, but registering is very simple and only takes a few moments.</p><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I register to be a user?<br/>
+<strong>A:</strong> Easy!  Just click on the "First Time User?" link next to the Reset button in the blue header bar above.
+Follow the instructions on that page and you'll be posting in no time!</p><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I change my password?<br/>
+<strong>A:</strong> First you must be logged in as a registered user.  Then you can click on "User Control Panel" in the blue header above.
+There will be a link on that page to change your password.</p><br/>
+
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> What if I want to use another email address with my user account?<br/>
+<strong>A:</strong> First you must be logged in as a registered user.  Then you can click on "User Control Panel" in the blue header above.
+There will be a link on that page to change your email address associated with your account.</p><br/>
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
