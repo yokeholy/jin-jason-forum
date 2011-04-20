0512fe41d7c2cf39abf5ddc87b5e1f9786707047
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-20 02:56:29 +0000

    Officially: We are in debugging phase!

--- trunk/index.php
+++ trunk/index.php
@@ -4,17 +4,5 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 
 <h1>Welcome to J2 Forum.</h1>
 <h2>created by Jin Wang and Jason Campbell</h2>
-<p>Functions that are working now:<br />
-* User Registration *<br />
-* Login and Logout *
-<br />
-* Post a new thread *<br />
-* Post a new reply *<br />
-* Sticky thread *
-<br />
-	* Forum threads list
-	* (working on list in descending order of &quot;last replied&quot; time)<br />
-* User Control Panel * (under working)</p>
-<?php
-include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
-?>
+<p>All functions are working now.</p>
+<p>We're in debugging phase.</p>
