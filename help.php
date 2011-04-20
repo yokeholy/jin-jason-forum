0b95fcdd18ad0964bfae9cd4be0ee7efafde5ac8
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-20 02:49:30 +0000

    more additions to the help page

--- help.php
+++ help.php
@@ -4,25 +4,37 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 
 <h1>Help</h1>
 
-<p class = "help" style="text-align:left; margin-top:10px"><strong>Q & A:</strong></p><br/><br/>
+<p class = "help" style="text-align:left; margin-top:10px; font-size:20px"><strong>Q & A:</strong></p><br/>
 
 <p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> Can I view forum posts without being a registered user?<br/>
 <strong>A:</strong> Of course!  Feel free to view and read as many posts as you would like.</p><br/>
 
-<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> Can I reply to a post without being a registered user?<br/>
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> Can I reply to a post without being a registered user?<br/>
 <strong>A:</strong> No, but registering is very simple and only takes a few moments.</p><br/>
 
-<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I register to be a user?<br/>
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I register to be a user?<br/>
 <strong>A:</strong> Easy!  Just click on the "First Time User?" link next to the Reset button in the blue header bar above.
 Follow the instructions on that page and you'll be posting in no time!</p><br/>
 
-<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I change my password?<br/>
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I change my password?<br/>
 <strong>A:</strong> First you must be logged in as a registered user.  Then you can click on "User Control Panel" in the blue header above.
 There will be a link on that page to change your password.</p><br/>
 
-<p class = "about" style="text-align:left; margin-top:10px"><strong>Q:</strong> What if I want to use another email address with my user account?<br/>
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> What if I want to use another email address with my user account?<br/>
 <strong>A:</strong> First you must be logged in as a registered user.  Then you can click on "User Control Panel" in the blue header above.
 There will be a link on that page to change your email address associated with your account.</p><br/>
+
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I reply to a thread?<br/>
+<strong>A:</strong> First you must be logged in as a registered user.  Then click on "Forum" in the blue header above.
+Next click on the thread to which you would like to reply.  Once you are viewing the thread, click on either of the "New Reply" buttons 
+(found at the top and bottom of the page).  This will take you to the New Reply page where you can enter your reply in the Content section, 
+then click the "Post the Reply!" button when you are finished entering your reply.</p><br/>
+
+<p class = "help" style="text-align:left; margin-top:10px"><strong>Q:</strong> How do I create a new thread?<br/>
+<strong>A:</strong> First you must be logged in as a registered user.  Then click on "Forum" in the blue header above.
+You will see the words "Add a Thread" at the top and bottom of the list of threads.  This will take you to the New Thread page 
+where you can enter the Subject and Content, then click the "Post the New Thread!" button when you are finished entering the new thread info.</p><br/>
+
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
