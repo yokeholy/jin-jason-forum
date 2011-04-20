65f80a59988d1760b3ef3e49d4ba6c11035bce0b
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-20 02:54:22 +0000

    ok, I've updated my own 'about' passage...

--- about.php
+++ about.php
@@ -3,15 +3,16 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 ?>
 
 <h1>About Us</h1>
-<p class = "about" style="text-align:left; margin-top:10px"><strong>Jin Wang</strong> is (Jin fill this in)</p><br/><br/>
+<p class = "about" style="text-align:left; margin-top:10px"><strong>Jin Wang</strong> is a Computer Science major senior student at Troy University with a weird combination of Music Industry minor. He has been working on several different websites by himself from personal blog to commercial online shopping system and on-web game.<br/>
+Jin is currently struggling of choosing his career path: computer or music. He wants to mix both but has no clue how to.<br/>
+The reason to start this project was simple: "It just came into my mind." Met with Jason on the very first day of the Web-based Programming course, Jin invited him to develop this forum together.</p>
 
 <p class = "about" style="text-align:left; margin-top:10px"><strong>Jason Campbell</strong> is a student in Computer Science at Troy University.  
 	He has a degree in Computer Engineering from North Carolina State University and has worked in computer hardware design at IBM.  
 	Back in his home town of Troy, AL, he is seeking a second degree to follow a more software oriented career.
 	Jason enjoys spending time with his wife and daughter, video games, guitar, tennis, golf, and of course, computer programming!</p><br/><br/>
 	
-<p class = "about" style="text-align:left; margin-top:10px">Together, <strong>Jin and Jason</strong> decided to put their skills to the test and create an
-	open forum to discuss any subject material comprehensive of all human knowledge!</p>
+<p class = "about" style="text-align:left; margin-top:10px">Together, <strong>Jin and Jason</strong> decided to put their skills to the test and create an	open forum to discuss any subject material comprehensive of all human knowledge!</p>
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
