f2938f50bf6513bf1d5871eab12169e740aa82dd
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 18:15:00 +0000

    block editied code in several files

--- trunk/functions/addpost_action.php
+++ trunk/functions/addpost_action.php
@@ -1,5 +1,6 @@
 <?php 
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
+
 if(!isset($_SESSION['LoggedIn']) || $_SESSION['LoggedIn'] != 1)
 {
 	echo('<p class = "error">Please login before posting a new thread or reply.</p>');
@@ -10,38 +11,37 @@ else if(!isset($_POST['Submit']) || $_POST['Submit'] != "Post!")
 }
 else
 {
-extract($_POST);
-extract($_SESSION);
-$Date = date('Y-m-d');
-$Time = date('H:i:s');
-$Subject = addslashes($Subject);
-$Content = addslashes($Content);
-if($NewType == 1) //reply process
-{
-	if(!mysql_query("INSERT INTO posts (ThreadID, Subject, PostDate, PostTime, PostContent, PostType, UserID) VALUES ('$tid', '$Subject', '$Date', '$Time', '$Content', '$NewType', '$UserID')"))
-	echo ('<p class="error">'.mysql_error().'</p>');
-	else
-	echo ('<p>Yeah! Your post is online now!</p>');
-}
-else // original post process
-{
-	if(!mysql_query("INSERT INTO posts (Subject, PostDate, PostTime, PostContent, PostType, UserID) VALUES ('$Subject', '$Date', '$Time', '$Content', '$NewType', '$UserID')"))
-	echo ('<p class="error">'.mysql_error().'</p>');
-	else
+	extract($_POST);
+	extract($_SESSION);
+	$Date = date('Y-m-d');
+	$Time = date('H:i:s');
+	$Subject = addslashes($Subject);
+	$Content = addslashes($Content);
+
+	if($NewType == 1) //reply process
 	{
-		$PostID = mysql_fetch_array(mysql_query("SELECT MAX(PostID) FROM posts"));
-		$pid = $PostID[0];
-		if(!mysql_query("UPDATE posts SET ThreadID = '$pid' WHERE PostID = '$pid'"))
-		echo ('<p class="error">'.mysql_error().'</p>');
+		if(!mysql_query("INSERT INTO posts (ThreadID, Subject, PostDate, PostTime, PostContent, PostType, UserID) VALUES ('$tid', '$Subject', '$Date', '$Time', '$Content', '$NewType', '$UserID')"))
+			echo ('<p class="error">'.mysql_error().'</p>');
 		else
-		echo ('<p>Yeah! Your post is online now!</p>');
+			echo ('<p>Yeah! Your post is online now!</p>');
 	}
-}
+	else // original post process
+	{
+		if(!mysql_query("INSERT INTO posts (Subject, PostDate, PostTime, PostContent, PostType, UserID) VALUES ('$Subject', '$Date', '$Time', '$Content', '$NewType', '$UserID')"))
+			echo ('<p class="error">'.mysql_error().'</p>');
+		else
+		{
+			$PostID = mysql_fetch_array(mysql_query("SELECT MAX(PostID) FROM posts"));
+			$pid = $PostID[0];
+			if(!mysql_query("UPDATE posts SET ThreadID = '$pid' WHERE PostID = '$pid'"))
+			echo ('<p class="error">'.mysql_error().'</p>');
+			else
+			echo ('<p>Yeah! Your post is online now!</p>');
+		}
+	}
 ?>
 
-
-
 <?php
-}
+} // end else
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
 ?>
--- trunk/functions/login_action.php
+++ trunk/functions/login_action.php
@@ -2,6 +2,7 @@
 include("{$_SERVER['DOCUMENT_ROOT']}/config/database.php");
 session_start();
 $_SESSION['SessionStarted'] = 1;
+
 if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
 {
 	include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
@@ -9,112 +10,81 @@ if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
 }
 else
 {
-extract($_POST);
+	extract($_POST);
 
-$passscram= md5($Password);
-$queryCheckForUsername = "SELECT UserName FROM users WHERE UserName = '".$UserName."'";
-$queryCheckForPassword = "SELECT UserName FROM users WHERE UserName = '".$UserName."' AND Password = '".$passscram."'";
+	$passscram= md5($Password);
+	$queryCheckForUsername = "SELECT UserName FROM users WHERE UserName = '".$UserName."'";
+	$queryCheckForPassword = "SELECT UserName FROM users WHERE UserName = '".$UserName."' AND Password = '".$passscram."'";
 
+	$_SESSION['LoggedIn'] = 0;
 
+	if(mysql_fetch_row(mysql_query($queryCheckForPassword)))
+	{	
+		// put variables from POST into SESSION (typed in when logging in)
+		$_SESSION['UserName'] = $UserName;
+		$_SESSION['Password'] = $Password;
 
+		// get UserID to use later if needed
+		$queryGetUserID = "SELECT UserID FROM users WHERE UserName = '".$UserName."'";
+		$row = (mysql_fetch_row(mysql_query($queryGetUserID)));
+		$UserID = $row[0];
+		$_SESSION['UserID'] = $UserID;
+		
+		// get EmailAddr to use in User Control Panel
+		$queryGetEmailAddr = "SELECT EmailAddr FROM users WHERE UserName = '".$UserName."'";
+		$row = (mysql_fetch_row(mysql_query($queryGetEmailAddr)));
+		$EmailAddr = $row[0];
+		$_SESSION['EmailAddr'] = $EmailAddr;
+		
+		// get OrigSignup to use in User Control Panel
+		$queryGetOrigSignup = "SELECT OrigSignup FROM users WHERE UserName = '".$UserName."'";
+		$row = (mysql_fetch_row(mysql_query($queryGetOrigSignup)));
+		$OrigSignup = $row[0];
+		$_SESSION['OrigSignup'] = $OrigSignup;
 
-/*
-$query = "SELECT * FROM users";
-$row = (mysql_fetch_row(mysql_query($query)));
-$a = $row[0];
-$b = $row[1];
-$c = $row[2];
-$d = $row[3];
-$e = $row[4];
-$f = $row[5];
+		// get LastLogin to use in User Control Panel
+		$queryGetLastLogin = "SELECT LastLogin FROM users WHERE UserName = '".$UserName."'";
+		$row = (mysql_fetch_row(mysql_query($queryGetLastLogin)));
+		$LastLogin = $row[0];
+		$_SESSION['LastLogin'] = $LastLogin;
+		
+		// get current date and time to update new LastLogin
+		$now = getdate();
+		$year = $now['year'];
+		$month = $now['mon'];
+		$day = $now['mday'];
+		$hours = $now['hours'];
+		$minutes = $now['minutes'];
+		$seconds = $now['seconds'];
 
-echo $a;
-echo $b;
-echo $c;
-echo $d;
-echo $e;
-echo $f;
-*/
+		$_SESSION['LoggedIn'] = 1;
+		include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
+		print("<p><span class=\"welcome\">Welcome, $UserName!  You have successfully logged in!</p>");
+		
+		$DateAndTime = "$year-$month-$day  $hours:$minutes:$seconds";
+		print("<br/><span class=\"welcome\">Previous login: " . $LastLogin . " (GMT)</span>");
 
-$_SESSION['LoggedIn'] = 0;
-
-if(mysql_fetch_row(mysql_query($queryCheckForPassword)))
-{	
-	// put variables from POST into SESSION (typed in when logging in)
-	$_SESSION['UserName'] = $UserName;
-	$_SESSION['Password'] = $Password;
-
-	// get UserID to use later if needed
-	$queryGetUserID = "SELECT UserID FROM users WHERE UserName = '".$UserName."'";
-	$row = (mysql_fetch_row(mysql_query($queryGetUserID)));
-	$UserID = $row[0];
-	$_SESSION['UserID'] = $UserID;
-	
-	// get EmailAddr to use in User Control Panel
-	$queryGetEmailAddr = "SELECT EmailAddr FROM users WHERE UserName = '".$UserName."'";
-	$row = (mysql_fetch_row(mysql_query($queryGetEmailAddr)));
-	$EmailAddr = $row[0];
-	$_SESSION['EmailAddr'] = $EmailAddr;
-	
-	// get OrigSignup to use in User Control Panel
-	$queryGetOrigSignup = "SELECT OrigSignup FROM users WHERE UserName = '".$UserName."'";
-	$row = (mysql_fetch_row(mysql_query($queryGetOrigSignup)));
-	$OrigSignup = $row[0];
-	$_SESSION['OrigSignup'] = $OrigSignup;
-
-	// get LastLogin to use in User Control Panel
-	$queryGetLastLogin = "SELECT LastLogin FROM users WHERE UserName = '".$UserName."'";
-	$row = (mysql_fetch_row(mysql_query($queryGetLastLogin)));
-	$LastLogin = $row[0];
-	$_SESSION['LastLogin'] = $LastLogin;
-	
-	// get current date and time to update new LastLogin
-	$now = getdate();
-	$year = $now['year'];
-	$month = $now['mon'];
-	$day = $now['mday'];
-	$hours = $now['hours'];
-	$minutes = $now['minutes'];
-	$seconds = $now['seconds'];
-
-	$_SESSION['LoggedIn'] = 1;
-	include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-	print("<p><span class=\"welcome\">Welcome, $UserName!  You have successfully logged in!</p>");
-	
-	$DateAndTime = "$year-$month-$day  $hours:$minutes:$seconds";
-	//print("<span class=\"congrats\">current date/time: " . $DateAndTime . " (GMT)</span>");	
-	print("<br/><span class=\"welcome\">Previous login: " . $LastLogin . " (GMT)</span>");
-
-	$queryUpdateLastLogin = "UPDATE users SET LastLogin = '".$DateAndTime."' WHERE UserName = '".$UserName."'";
-	mysql_query($queryUpdateLastLogin);
-	
-
-}
-else if(mysql_fetch_row(mysql_query($queryCheckForUsername)) && !mysql_fetch_row(mysql_query($queryCheckForPassword)))
-{
-	include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-	print("<p><span class = 'error'>
-	Incorrect Password.</span><br /><br />
-	Re-enter Username and Password and try again.<br /><br />
-	Thank You.</span></p>");
-	die(include("{$_SERVER['DOCUMENT_ROOT']}footer.php")); // terminate script execution			
-}
-else
-{
-	include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-	print("<p><span class = 'error'>
-	Username does not exist.</span><br /><br />
-	Re-enter a valid Username and try again, or click \"First Time User?\" above.<br /><br />
-	Thank You.</span></p>");
-	die(include("{$_SERVER['DOCUMENT_ROOT']}footer.php")); // terminate script execution			
-}
-
-// this line can be taken out eventually
-//echo ('<p>Your username input is: '.$_POST['UserName'].'<br />And your password input is: '.MD5($_POST['Password']).'(MD5 hashed)</p>');
-
-
-//echo $UserName;
-
+		$queryUpdateLastLogin = "UPDATE users SET LastLogin = '".$DateAndTime."' WHERE UserName = '".$UserName."'";
+		mysql_query($queryUpdateLastLogin);
+	}
+	else if(mysql_fetch_row(mysql_query($queryCheckForUsername)) && !mysql_fetch_row(mysql_query($queryCheckForPassword)))
+	{
+		include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
+		print("<p><span class = 'error'>
+		Incorrect Password.</span><br /><br />
+		Re-enter Username and Password and try again.<br /><br />
+		Thank You.</span></p>");
+		die(include("{$_SERVER['DOCUMENT_ROOT']}footer.php")); // terminate script execution			
+	}
+	else
+	{
+		include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
+		print("<p><span class = 'error'>
+		Username does not exist.</span><br /><br />
+		Re-enter a valid Username and try again, or click \"First Time User?\" above.<br /><br />
+		Thank You.</span></p>");
+		die(include("{$_SERVER['DOCUMENT_ROOT']}footer.php")); // terminate script execution			
+	}
 }
 
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
