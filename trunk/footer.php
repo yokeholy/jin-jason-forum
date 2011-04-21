5630ca1cdcfffbb2661368b5f7b180a1d93dba86
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 17:56:55 +0000

    block editied code in several files

--- trunk/footer.php
+++ trunk/footer.php
@@ -1,10 +1,11 @@
-﻿</td>
-<tr>
-<td colspan="2" align="center">
-<hr  width="100%"/>
-<p>Copyright Reserved, Jin Wang and Jason Campbell, 2011</p></td>
-</tr>
-</table>
+﻿				</td>
+			<tr>
+				<td colspan="2" align="center">
+					<hr  width="100%"/>
+					<p>Copyright Reserved, Jin Wang and Jason Campbell, 2011</p>
+				</td>
+			</tr>
+		</table>
 
-</body>
+	</body>
 </html>
--- trunk/newpost.php
+++ trunk/newpost.php
@@ -6,54 +6,55 @@ if(!isset($_SESSION['LoggedIn']) || $_SESSION['LoggedIn'] != 1)
 }
 else
 {
-extract($_POST);
-if(isset($NewType) && $NewType == 1) // If the new post is a reply to an existing thread. NewType 1 means that this post is a reply and will cause the page to show the New Reply interface. Otherwise, it's gonna show the New Thread interface.
-{
+	extract($_POST);
+	// If the new post is a reply to an existing thread. NewType 1 means that this post is a reply and will cause 
+	// the page to show the New Reply interface. Otherwise, it's gonna show the New Thread interface.
+	if(isset($NewType) && $NewType == 1)
+	{
 ?>
+		<h1 align="left">New Reply</h1>
+		<form action="/functions/addpost_action.php" method="post">
+			<input name="NewType" type="hidden" value="1" />
+			<input name="tid" type="hidden" value="<?php echo $tid ?>" />
 
-<h1 align="left">New Reply</h1>
-<form action="/functions/addpost_action.php" method="post">
-<input name="NewType" type="hidden" value="1" />
-<input name="tid" type="hidden" value="<?php echo $tid ?>" />
-
-	<table cellpadding="0" cellspacing="0" class="AddPostTable">
-		<tr>
-			<td class="AddPost"><p><strong>Subject:</strong></p></td>
-			<td class="AddPost"><input name="Subject" size="75" value="Re: <?php echo $subject ?>" /></td>
-		</tr>
-		<tr>
-			<td class="AddPost"><p><strong>Content:</strong></p></td>
-			<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
-		</tr>
-		<tr align="center">
-			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post!" /><input type="reset" value="Clear All Entries"></td>
-		</tr>
-	</table>
-</form>
+			<table cellpadding="0" cellspacing="0" class="AddPostTable">
+				<tr>
+					<td class="AddPost"><p><strong>Subject:</strong></p></td>
+					<td class="AddPost"><input name="Subject" size="75" value="Re: <?php echo $subject ?>" /></td>
+				</tr>
+				<tr>
+					<td class="AddPost"><p><strong>Content:</strong></p></td>
+					<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
+				</tr>
+				<tr align="center">
+					<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post!" /><input type="reset" value="Clear All Entries"></td>
+				</tr>
+			</table>
+		</form>
 <?php
-}
-else
-{
+	}
+	else
+	{
 ?>
-<h1 align="left">New Thread</h1>
-<form action="/functions/addpost_action.php" method="post">
-<input name="NewType" type="hidden" value="0" />
-	<table cellpadding="0" cellspacing="0" class="AddPostTable">
-		<tr>
-			<td class="AddPost"><p><strong>Subject:</strong></p></td>
-			<td class="AddPost"><input name="Subject" size="75"></td>
-		</tr>
-		<tr>
-			<td class="AddPost"><p><strong>Content:</strong></p></td>
-			<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
-		</tr>
-		<tr align="center">
-			<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post!"><input type="reset" value="Clear All Entries"></td>
-		</tr>
-	</table>
-</form>
+		<h1 align="left">New Thread</h1>
+		<form action="/functions/addpost_action.php" method="post">
+		<input name="NewType" type="hidden" value="0" />
+			<table cellpadding="0" cellspacing="0" class="AddPostTable">
+				<tr>
+					<td class="AddPost"><p><strong>Subject:</strong></p></td>
+					<td class="AddPost"><input name="Subject" size="75"></td>
+				</tr>
+				<tr>
+					<td class="AddPost"><p><strong>Content:</strong></p></td>
+					<td class="AddPost"><textarea name="Content" cols="100" rows="20"></textarea></td>
+				</tr>
+				<tr align="center">
+					<td colspan="2" class="AddPost"><input name="Submit" type="submit" value="Post!"><input type="reset" value="Clear All Entries"></td>
+				</tr>
+			</table>
+		</form>
 <?php
-}
+	}
 }
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
 ?>
