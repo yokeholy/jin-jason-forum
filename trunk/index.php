bd00c46b00df277bdd031ec91aa8cd1d16ebf150
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 23:20:26 +0000

    added quote to home page

--- trunk/index.php
+++ trunk/index.php
@@ -4,8 +4,8 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 
 <h1>Welcome to J2 Forum.</h1>
 <h2>created by Jin Wang and Jason Campbell</h2>
-<p>All functions are working now.</p>
-<p>We're in debugging phase.</p>
+<p class = "log" style = "font-size:20px"><br/>"It just came into my mind."</p>
+
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
