e4df73542a5adfd43618fa8e5764bf62144ebd46
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 18:09:11 +0000

    block editied code in several files

--- update_password.php
+++ update_password.php
@@ -1,32 +1,30 @@
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-//session_start();
 ?>
 
-		<h1 style="text-align:left; margin-top:10px"><strong>Change Password</strong></h1>
-		<form name="ChangePassword" action="update_password_action.php" method="post" style="text-align:left">
-		<table width="100%" align="left" class="control_panel">
-			<tr>
-				<td>Confirm Old Password: &nbsp;</td>
-				<td><input name="ConfirmOldPassword" type="password" size="20" maxlength="20" /> <span class="instructions"></span></td>
-			</tr>
-			<tr>
-				<td>New Password: &nbsp;</td>
-				<td><input name="NewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
-			</tr>
-			<tr>
-				<td width="200">Confirm New Password: &nbsp;</td>
-				<td><input name="ConfirmNewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
-			</tr>
-
-			<tr>
-				<td colspan="2">
-					<input type="submit" value="Submit" />
-					<input type="reset" value="Reset" />
-				</td>
-			</tr>			
-		</table>
-		</form>
+	<h1 style="text-align:left; margin-top:10px"><strong>Change Password</strong></h1>
+	<form name="ChangePassword" action="update_password_action.php" method="post" style="text-align:left">
+	<table width="100%" align="left" class="control_panel">
+		<tr>
+			<td>Confirm Old Password: &nbsp;</td>
+			<td><input name="ConfirmOldPassword" type="password" size="20" maxlength="20" /> <span class="instructions"></span></td>
+		</tr>
+		<tr>
+			<td>New Password: &nbsp;</td>
+			<td><input name="NewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
+		</tr>
+		<tr>
+			<td width="200">Confirm New Password: &nbsp;</td>
+			<td><input name="ConfirmNewPassword" type="password" size="20" maxlength="20" /> <span class="instructions"> (6 to 20 characters)</span></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<input type="submit" value="Submit" />
+				<input type="reset" value="Reset" />
+			</td>
+		</tr>			
+	</table>
+	</form>
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
--- update_password_action.php
+++ update_password_action.php
@@ -32,85 +32,43 @@ else
 	else
 		$ConfirmNewPasswordIsBlank = 0;
 		
-		
 	$PasswordError = 0;
-
 		
 	// if all fields are blank
 	if($ConfirmOldPasswordIsBlank && $NewPasswordIsBlank && $ConfirmNewPasswordIsBlank)
 	{
 		$PasswordError = 1;
 		$_SESSION['PasswordError'] = $PasswordError; 
-		/*
-		print("<p><span class = 'error'>
-		No changes requested.</span><br /><br />
-		- Click the Back button to return to Change Password screen<br /><br />
-		- Or feel free to go to other areas of the Forum!");
-		*/
 		die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 	}
-	
 	// if confirm old password is blank and any other fields have entries
 	else if($ConfirmOldPasswordIsBlank && (!$NewPasswordIsBlank || !$ConfirmNewPasswordIsBlank))
 	{
 		$PasswordError = 2;
 		$_SESSION['PasswordError'] = $PasswordError;
-		/*
-		print("<p><span class = 'error'>
-		Current password must be confirmed to make changes.</span><br /><br />
-		- Click the Back button to return to Change Password screen<br /><br />
-		- Be sure Confirm Old Password matches your current password<br />
-		before making changes");
-		*/
 		die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 	}
-		
 	// if confirm old password (is not blank and) does not match current password
 	else if(!$ConfirmOldPasswordIsBlank && ($Password != $ConfirmOldPassword))
 	{
 		$PasswordError = 3;
 		$_SESSION['PasswordError'] = $PasswordError;
-		/*
-		print("<p><span class = 'error'>
-		Current Password mismatch.</span><br /><br />
-		- Click the Back button<br />
-		- Make sure the Confirm Old Password field matches your current password exactly<br />
-		- Then resubmit.<br /><br />
-		Thank You.</span></p>");
-		*/
 		die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 	}
-	
 	// if confirm old password is not blank and matches current password, and all other fields are blank
 	else if(!$ConfirmOldPasswordIsBlank && $NewPasswordIsBlank && $ConfirmNewPasswordIsBlank)
 	{
 		$PasswordError = 4;
 		$_SESSION['PasswordError'] = $PasswordError;
-		/*
-		print("<p><span class = 'error'>
-		Current password confirmed but no changes requested.</span><br /><br />
-		- Click the Back button to return to Change Password screen<br /><br />
-		- Or feel free to go to other areas of the Forum!");
-		*/
 		die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 	}
-		
 	// if confirm old password matches current password, and New Password and Confirm New Password don't match		
 	else if(!$ConfirmOldPasswordIsBlank && ((!$NewPasswordIsBlank || !$ConfirmNewPasswordIsBlank) && $NewPassword != $ConfirmNewPassword))
 	{
 		$PasswordError = 5;
 		$_SESSION['PasswordError'] = $PasswordError;
-		/*
-		print("<p><span class = 'error'>
-		New Password mismatch.</span><br /><br />
-		- Click the Back button<br />
-		- Make sure the New Password and Confirm New Password fields match exactly<br />
-		- Then resubmit.<br /><br />
-		Thank You.</span></p>");
-		*/
 		die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 	}
-	
 	// if confirm old password matches current password, and New Password matches Confirm New Password, but don't conform to correct password format
 	else if(!$ConfirmOldPasswordIsBlank && ((!$NewPasswordIsBlank || !$ConfirmNewPasswordIsBlank) && $NewPassword == $ConfirmNewPassword))
 	{
@@ -118,16 +76,6 @@ else
 		{
 			$PasswordError = 6;
 			$_SESSION['PasswordError'] = $PasswordError;
-			/*
-			print("<p><span class = 'error'>
-			Invalid New Password format.</span><br /><br />
-			A valid Password must:<br />
-			- Contain at least 6 characters and not exceed 20 characters<br />
-			- Not contain spaces<br />
-			- Only contain alphabetic characters, digits, underscores, or hyphens<br /><br />
-			Click the Back button, enter a valid New Password, then resubmit.<br /><br />
-			Thank You.</span></p>");
-			*/
 			die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 		}
 		else
@@ -141,9 +89,6 @@ else
 			//mysql_query($queryUpdatePassword);
 			//$_SESSION['Password'] = $NewPassword;
 			
-			// THIS ONE MUST BE COMMENTED PERMANENTLY
-			//print("Password has been successfully changed! NOT REALLY, the DATABASE UPDATE IS CURRENTLY COMMENTED OUT");
-
 			die(include("{$_SERVER['DOCUMENT_ROOT']}/update_password_action_error.php")); // terminate script execution
 		}
 	}
