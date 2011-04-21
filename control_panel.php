05667bad9d9c3794bb035ac232e05e03c8f9f301
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 17:56:55 +0000

    block editied code in several files

--- control_panel.php
+++ control_panel.php
@@ -1,64 +1,50 @@
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
-//session_start();
+//session_start(); // not needed here b/c in header.php already
 ?>
 
 <?php
 	
+if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
+{
+	$UserName = $_SESSION['UserName'];
+	$Password = $_SESSION['Password'];
+	$EmailAddr = $_SESSION['EmailAddr'];
+	$OrigSignup = $_SESSION['OrigSignup'];
 	
-	if(isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == 1)
-	{
-				
-		$UserName = $_SESSION['UserName'];
-		$Password = $_SESSION['Password'];
-		$EmailAddr = $_SESSION['EmailAddr'];
-		$OrigSignup = $_SESSION['OrigSignup'];
-		
-		$UserID = $_SESSION['UserID'];
-		$LastLogin = $_SESSION['LastLogin'];
-		//echo $UserName;
-		//echo $Password;
-		//echo $EmailAddr;
-		
-	//	$getEmailAddr = "SELECT EmailAddr FROM users WHERE UserName = '".$UserName."'";
-	//	$EmailAddr = mysql_fetch_row(mysql_query($getEmailAddr));
+	$UserID = $_SESSION['UserID'];
+	$LastLogin = $_SESSION['LastLogin'];
 
 	// get date and time:
-		$now = getdate();
+	$now = getdate();
+	$year = $now['year'];
+	$month = $now['mon'];
+	$day = $now['mday'];
+	$hours = $now['hours'];
+	$minutes = $now['minutes'];
+	$seconds = $now['seconds'];
+	
+	$dateAndTime = "<p>$year-$month-$day  $hours:$minutes:$seconds</p>";
+	
+	// get Total Posts to display in User Control Panel
+	$queryGetTotalPosts = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."'";
+	$result = mysql_query($queryGetTotalPosts);
+	$numRows = mysql_fetch_array($result);
+	$TotalPosts = $numRows[0];
 
-		$year = $now['year'];
-		$month = $now['mon'];
-		$day = $now['mday'];
-		$hours = $now['hours'];
-		$minutes = $now['minutes'];
-		$seconds = $now['seconds'];
-		
-		//print("<p>$year-$month-$day  $hours:$minutes:$seconds</p>");
-
-		$dateAndTime = "<p>$year-$month-$day  $hours:$minutes:$seconds</p>";
-		//print("Last login: " . $LastLogin);
-		
-		// get Total Posts to display in User Control Panel
-		$queryGetTotalPosts = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."'";
-		$result = mysql_query($queryGetTotalPosts);
-		$numRows = mysql_fetch_array($result);
-		$TotalPosts = $numRows[0];
-
-		// get Threads Started to display in User Control Panel
-		$queryGetThreadsStarted = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."' AND PostType = 0";
-		$result = mysql_query($queryGetThreadsStarted);
-		$numRows = mysql_fetch_array($result);
-		$ThreadsStarted = $numRows[0];
-
-
+	// get Threads Started to display in User Control Panel
+	$queryGetThreadsStarted = "SELECT COUNT(*) FROM posts WHERE UserID = '".$UserID."' AND PostType = 0";
+	$result = mysql_query($queryGetThreadsStarted);
+	$numRows = mysql_fetch_array($result);
+	$ThreadsStarted = $numRows[0];
 ?>
 
 
-		<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
-		<h3 style="text-align:left; margin-top:10px">Welcome, <?php print($UserName);?>!  You can change view and change your info here.</h3>
-		
-		<div class="control_panel" style="text-align:left; margin-top:10px"/>Current User Info:</div>
-		<form name="ChangeOptions" style="text-align:left">
+	<h1 style="text-align:left; margin-top:10px"><strong>User Control Panel</strong></h1>
+	<h3 style="text-align:left; margin-top:10px">Welcome, <?php print($UserName);?>!  You can change view and change your info here.</h3>
+	
+	<div class="control_panel" style="text-align:left; margin-top:10px"/>Current User Info:</div>
+	<form name="ChangeOptions" style="text-align:left">
 		<table width="100%" align="left" class="control_panel">
 			<tr>
 				<td>Username: &nbsp;</td>
@@ -84,23 +70,6 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 				<td>Threads Started: &nbsp;</td>
 				<td><?php print($ThreadsStarted);?></td>
 			</tr>			
-<!--
-			<tr>
-				<td>Threads per page: &nbsp;</td>
-				<td><select name="ThreadsPerPage" >
-						<option selected = "selected">10</option>
-						<option>25</option>
-						<option>50</option>
-						<option>100</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>Thread Order: &nbsp;</td>
-				<td><label>Newest First <input name="ThreadOrder" type="checkbox" value="NewestFirst"/> </label>
-					<label>Oldest First <input name="ThreadOrder" type="checkbox" value="OldestFirst"/> </label></td>
-			</tr>
--->
 			<tr>
 				<td><br/></td>
 			</tr>
@@ -114,9 +83,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 				<td width="220"><a href="/update_email_addr.php" class="change_password">Update Email Address? (click here)</a></td>
 			</tr>
 		</table>
-		</form>
-
-	
+	</form>
 
 <?php
 } // end if (checking if LoggedIn is valid)
