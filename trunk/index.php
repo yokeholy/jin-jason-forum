bd1b3276ee05a5df9b18c3c439c1eac10dedada3
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-21 23:21:43 +0000

    edited quote on home page

--- trunk/index.php
+++ trunk/index.php
@@ -4,7 +4,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 
 <h1>Welcome to J2 Forum.</h1>
 <h2>created by Jin Wang and Jason Campbell</h2>
-<p class = "log" style = "font-size:20px"><br/>"It just came into my mind."</p>
+<p class = "log" style = "font-size:20px"><br/>"The thought just came into my mind."</p>
 
 
 <?php
