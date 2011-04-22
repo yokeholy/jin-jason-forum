3459e93f6eeeba4c27fbebb8e2ee8d28568723ed
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-22 01:38:16 +0000

    added constants.php to store constants needed

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+//MOST IMPORTANTLY, THE DATABASE CONNECTION VALUES
+define("HOST", "localhost"); //Host address, by default, "localhost"
+define("USER", "root"); //Username for database connection
+define("PASS", ""); //Password for database connection
+define("DATABASE", "forum"); //Database name used for this forum
+
+?>
