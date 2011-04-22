13cc9e1cf7ed621b236818b0088f8e4ab9d011ee
ncstatered@gmail.com <ncstatered@gmail.com@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-22 02:05:47 +0000

    added another power point slide that was needed

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 include("{$_SERVER['DOCUMENT_ROOT']}/header.php");
 ?>
 
-<span class = "log">You are logged out.</span>
+<span class = "about">You are logged out.</span>
 
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/footer.php");
