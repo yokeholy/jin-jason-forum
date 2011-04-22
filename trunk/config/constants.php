b092a66821fc5eaa16ab482f8ff52b9e8d7d070f
wangjin@wangjin.me <wangjin@wangjin.me@e87875d5-d12b-30a1-02d4-ec5be682c3ff>
2011-04-22 01:43:28 +0000

    check if constants are defined, or define them.

--- trunk/config/constants.php
+++ trunk/config/constants.php
@@ -1,8 +1,14 @@
 <?php
+if(!defined("CONSTANTS"))
+{
+	define("CONSTANTS", "");
 //MOST IMPORTANTLY, THE DATABASE CONNECTION VALUES
-define("HOST", "localhost"); //Host address, by default, "localhost"
-define("USER", "root"); //Username for database connection
-define("PASS", ""); //Password for database connection
-define("DATABASE", "forum"); //Database name used for this forum
+	define("HOST", "localhost"); //Host address, by default, "localhost"
+	define("USER", "root"); //Username for database connection
+	define("PASS", ""); //Password for database connection
+	define("DATABASE", "forum"); //Database name used for this forum
 
+//FORUM FUNCTIONALITY
+	define("HOT", 5);
+}
 ?>
